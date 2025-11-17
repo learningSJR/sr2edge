5bcc348dd864bb94abd752f8330f113d1816426d
= <learningsjr@gmail.com>
2025-11-17 20:13:51 +0530

    your commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">SREDGE</div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,tsx,ts}",
+    "./app/**/*.{js,jsx,tsx,ts}",
+    "./components/**/*.{js,jsx,tsx,ts}",
+  ],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
